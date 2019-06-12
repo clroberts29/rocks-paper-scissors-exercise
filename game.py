13f324bb79950fb7eaca59698f7671b94353e261
clroberts29 <50728923+clroberts29@users.noreply.github.com>
2019-06-11 21:00:21 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,16 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+# CAPTURE INPUTS
+user_choice = input("Please select one of the following options: ROCK, PAPER, or SCISSORS")
+     
+print("YOUR CHOICE IS: " + user_choice)
+
+# VALIDATE INPUTS
+
+# GENERATE COMPUTER SELECTION
+
+# DETERMINE THE WINNER
+
+# DISPLAY FINAL OUTPUTS / OUTCOMES
